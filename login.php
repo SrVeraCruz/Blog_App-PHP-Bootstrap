0e62a6d2209903515f5b594d06b61baec80820cc
Dúdú Vera Cruz <130259216+SrVeraCruz@users.noreply.github.com>
2024-04-20 03:29:24 +0100

    Login/Register feautures

---
+++ login.php
@@ -0,0 +1,43 @@
+<?php 
+  session_start();
+  include('includes/header.php');
+  include('includes/navbar.php')
+?>
+
+<div class="py-5">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-5">
+
+        <?php include('message.php') ?>
+
+        <div class="card">
+          <div class="card-header">
+            <h3>Hello</h3>
+          </div>
+          <div class="card-body">
+
+          <form action="logincode.php" method="post">
+            <div class="form-group mb-3">
+              <label>Email</label>
+              <input type="email" name="email" required placeholder="Email Adderess" class="form-control">
+            </div>
+            <div class="form-group mb-3">
+              <label>Password</label>
+              <input type="password" name="password" required placeholder="Password" class="form-control">
+            </div>
+            <div class="form-group mb-3">
+              <button type="submit" name="login_btn" class="btn btn-primary">Login Now</button>
+            </div>
+          </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php 
+  include('includes/footer.php')
+?>
---
+++ register.php
@@ -0,0 +1,55 @@
+<?php 
+  session_start();
+  include('includes/header.php');
+  include('includes/navbar.php')
+?>
+
+<div class="py-5">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-5">
+
+        <?php include('message.php') ?>
+
+        <div class="card">
+          <div class="card-header">
+            <h3>Register</h3>
+          </div>
+          <div class="card-body">
+
+            <form action="registercode.php" method="post" >
+              <div class="form-group mb-3">
+                <label>Firt Name</label>
+                <input type="text" name="fname" required placeholder="Firt Name" class="form-control">
+              </div>
+              <div class="form-group mb-3">
+                <label>Last Name</label>
+                <input type="text" name="lname" required placeholder="Last Name" class="form-control">
+              </div>
+              <div class="form-group mb-3">
+                <label>Email</label>
+                <input type="email" name="email" required placeholder="Email" class="form-control">
+              </div>
+              <div class="form-group mb-3">
+                <label>Password</label>
+                <input type="password" name="password" required placeholder="Password" class="form-control">
+              </div>
+              <div class="form-group mb-3">
+                <label>Confirm Password</label>
+                <input type="password" name="cpassword" required placeholder="Confirm Password" class="form-control">
+              </div>
+              <div class="form-group mb-3">
+                <button type="submit" name="register_btn" class="btn btn-primary">Login Now</button>
+              </div>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php 
+  include('includes/footer.php')
+?>
